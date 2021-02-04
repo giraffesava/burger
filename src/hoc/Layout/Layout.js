132b116c15ae57ccd6a8d047f31e5976e0140547
Savelii <savely54@inbox.ru>
2021-02-04 16:50:02 +0700

    added styling

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import Aux from '../Auxx/Auxx'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
+import classes from './Layout.module.css'
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
+
+class Layout extends Component {
+  state ={
+    showSideDrawer: true
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+  } 
+
+  sideDrawerToggleHandle = () => {
+    this.setState(prevState => {
+      return {showSideDrawer: !prevState.showSideDrawer}
+    } )
+  }
+
+  render(){
+    return (
+      <Aux>
+        <Toolbar 
+          drawerToggleClicked={this.sideDrawerToggleHandle} />
+        <SideDrawer 
+          open={this.state.showSideDrawer} 
+          closed={this.sideDrawerClosedHandler} 
+          />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+} 
+
+export default Layout 
