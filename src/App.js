e734dafec45e268bcd5c7dc3f306033a4c1b8457
Savelii <savely54@inbox.ru>
2021-02-05 14:11:51 +0700

    Removing old interceptors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-function App() {
-  return (
-    <Layout>
-      <BurgerBuilder />
-    </Layout>
-  );
+class App extends Component {
+
+  render(){
+    return (
+      <Layout>
+        {this.state.show ? <BurgerBuilder /> : null}
+      </Layout>
+    );
+  }
 }
 
 
