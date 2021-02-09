dcc3ff51fede34640c555f048b1e5ea76879ab79
Savelii <savely54@inbox.ru>
2021-02-09 23:19:09 +0700

    Created checkout summary

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 import './App.css';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import Checkout from './containers/Checkout/Checkout'
+
 class App extends Component {
 
   render(){
     return (
       <Layout>
         <BurgerBuilder />
+        <Checkout />
       </Layout>
     );
   }
