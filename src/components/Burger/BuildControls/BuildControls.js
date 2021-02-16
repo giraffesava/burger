235773f558d9fb757e42165b90828516fef5b6b6
Savelii <savely54@inbox.ru>
2021-02-16 23:48:33 +0700

    added Redux

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,12 +12,15 @@ const controls = [
 const BuildControls = (props) => (
     <div className={classes.BuildControls}>
       <p>Current price: <strong>{props.price.toFixed(2)}$</strong></p>
-      {controls.map(ctrl => {
-        return <BuildControl key={ctrl.label} label={ctrl.label} added={() => props.ingredientAdded(ctrl.type)}
-        removed={()=> props.ingredientRemoved(ctrl.type)}
-        disabled={props.disabled[ctrl.type]}
+      {controls.map(ctrl => (
+        <BuildControl 
+          key={ctrl.label} 
+          label={ctrl.label} 
+          added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemoved(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
         />
-    })}
+    ))}
     <button 
     className={classes.OrderButton}
     disabled={!props.purchasable}
