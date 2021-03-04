24b315f85f98110897a5da5cc696dc8b2b215eac
Savelii <savely54@inbox.ru>
2021-03-05 03:42:53 +0700

    optimization

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,10 +3,6 @@ import Aux from '../../../hoc/Auxx/Auxx'
 import Button from '../../UI/Button/Button'
 
 class OrderSummary extends Component {
-  componentDidUpdate(){
-    console.log('[OrderSummary] WillUpdate')
-  }
-
   render(){
     const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,7 +2,6 @@ import React from 'react'
 import classes from './Order.module.css'
 
 const order = (props) => {
-  console.log(props)
   const ingredients =[]
 
   for (let ingredientName in props.ingredients){
