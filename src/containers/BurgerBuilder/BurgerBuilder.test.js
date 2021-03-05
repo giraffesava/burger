26a83c6f10a9772d198c28fa86badab69c42511e
Savelii <savely54@inbox.ru>
2021-03-05 17:45:04 +0700

    testing

---
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import {BurgerBuilder} from './BurgerBuilder'
+import { configure, shallow } from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+configure({adapter: new Adapter()})
+
+describe('<BurgerBuilder />', () => {
+  let wrapper;
+
+  beforeEach(()=> {
+    wrapper = shallow(<BurgerBuilder onInitIngredients={()=>{}} />)
+  })
+
+  it('should reander <BuildControls /> when receiving ingredients', () => {
+    wrapper.setProps({ings: {salad: 0}})
+    expect(wrapper.find(BuildControls)).toHaveLength(1)
+  })
+})
